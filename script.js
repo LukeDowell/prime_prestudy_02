03c913d14e60c16aad4483d427c4b22ab299df85
Luke Dowell <lukedowell@gmail.com>
2015-07-03 18:38:44 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lukedowell on 7/3/15.
+ */
+avengersMovie = {
+    name: "Avengers",
+    runtime: 143,
+    releaseYear: 2012,
+    director: "Joss Whedon"
+};
+
+console.log(avengersMovie);
